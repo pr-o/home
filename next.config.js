ef4616b012362f4ea2c266714d5da3c2b9cc5ca8
pr-o <sunghahh.git@gmail.com>
2022-12-04 14:32:18 +0900

    initial setups and configurations

--- next.config.js
+++ next.config.js
@@ -1,7 +1,34 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 
-module.exports = nextConfig
+const withVanillaExtract = createVanillaExtractPlugin();
+
+module.exports = (phase, { defaultConfig }) => {
+  const nextConfig = { reactStrictMode: true, swcMinify: true };
+
+  const webpackConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.module.rules.push({
+        test: /\.(glsl)$/,
+        use: ['glslify-import-loader', 'raw-loader', 'glslify-loader'],
+      });
+
+      return config;
+    },
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...nextConfig,
+      ...withVanillaExtract(webpackConfig),
+      experimental: { esmExternals: false },
+    };
+  }
+
+  return {
+    ...nextConfig,
+    ...withVanillaExtract(webpackConfig),
+  };
+};
---
+++ src/styles/globals.css.ts
@@ -0,0 +1,12 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('html, body', {
+  padding: 0,
+  margin: 0,
+  fontFamily:
+    '-apple-system, BlinkMacSystemFont, Segoe UI, Ubuntu, Cantarell, Helvetica Neue, sans-serif',
+});
+
+globalStyle('a', { color: 'inherit', textDecoration: 'none' });
+
+globalStyle('*', { boxSizing: 'border-box' });
